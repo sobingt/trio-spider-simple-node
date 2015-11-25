bb6c8c137808d6718e223ce8f271a5ff9bf16bdb
Sobin George Thomas <sobingt@gmail.com>
2015-11-25 13:08:18 +0530

    Created static blog

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var app = express();
 
+app.set('views',__dirname+"/views");
+app.set('view engine','jade');
 //Routes
 app.get('/',function(req, res){
-  res.json({
-    name: "Surya"
-  });
+  res.render('home');
 });
+app.get('/blog1',function(req, res){
+  res.render('blogs/blog1');
+});
+app.get('/blog2',function(req, res){
+  res.render('blogs/blog2');
+});
+app.get('/blog3',function(req, res){
+  res.render('blogs/blog3');
+});
+app.get('/blog4',function(req, res){
+  res.render('blogs/blog4');
+});
+
 app.post('/',function(req, res){
   res.send("POST Method: Hello World");
 });
